3cdd1403684f8d9886f2e19828a344f5bfef93c9
Milan Gruner <milangruner@gmail.com>
2023-09-17 11:31:31 +0200

    Setup mdx support
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import yaml from '@rollup/plugin-yaml';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gruner.tech',
   vite: {
-    plugins: [yaml()],
+    plugins: [yaml()]
   },
+  integrations: [mdx()]
 });
